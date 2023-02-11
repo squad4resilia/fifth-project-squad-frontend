630fd1a05a76dfae549c696ac7c135d471fd22cb
Laririn <larissa98pereira@gmail.com>
2023-02-11 15:39:18 -0300

    criando componentes

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Page from './landingpage'
+import Reply from './replypage'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Page />
+    <Page/>
   </React.StrictMode>
 )
---
+++ src/replypage.jsx
@@ -0,0 +1,43 @@
+import anonymousLogo from './assets/anonymous.png'
+import "./replypage.css";
+
+function Reply() {
+    return (
+      <>
+      <div class="container">
+    <div class="imagem">
+        <img src={anonymousLogo} alt=""/>
+    </div>
+
+  <div class="wrapper">
+    <div class="input-box">
+      <div class="tweet-area">
+        <span class="placeholder">O que é que há, velhinho?</span>
+        <div class="input editable" contenteditable="true" spellcheck="false"></div>
+        <div class="input readonly" contenteditable="true" spellcheck="false"></div>
+      </div>
+      <div class="privacy">
+        <i class="fas fa-globe-asia"></i>
+        <span>Qualquer pessoa pode responder</span>
+      </div>
+    </div>
+    <div class="bottom">
+      <ul class="icons">
+        <li><i class="uil uil-capture"></i></li>
+        <li><i class="far fa-file-image"></i></li>
+        <li><i class="fas fa-map-marker-alt"></i></li>
+        <li><i class="far fa-grin"></i></li>
+        <li><i class="far fa-user"></i></li>
+      </ul>
+      <div class="content">
+        <span class="counter">100</span>
+        <button>Enviar</button>
+      </div>
+    </div>
+  </div>
+</div>
+      </>
+    );
+  }
+  
+  export default Reply;
