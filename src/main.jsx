4323ff6a5c2cee2067e45dc0282c4c7bcb22c8a1
PogeYN <yanalves1000@hotmail.com>
2023-02-12 12:08:33 -0300

    thread pag

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Page from './landingpage'
-import Reply from './replypage'
-import './index.css'
+import Thread from './thread-form' 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
--- src/thread-form.jsx
+++ src/thread-form.jsx
@@ -1,11 +1,18 @@
 import "./thread-form.css";
+import anonymousLogo from "./assets/anonymous.png";
 
 function ThreadForm() {
-    return (
-      <>
+  return (
+    <>
+      <div className="tudo">
+        <div className="imagem">
+          <img src={anonymousLogo} id="imagem" alt="" />
+        </div>
+        <button className="new">Novas publicações</button>
+      <input type="button" />
+      </div>
+    </>
+  );
+}
 
-      </>
-    );
-  }
-  
-  export default ThreadForm;
+export default ThreadForm;
