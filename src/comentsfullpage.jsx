4adbe2b02da31ad638a847d833bacd3d9c3da8b2
PogeYN <yanalves1000@hotmail.com>
2023-02-14 14:31:24 -0300

    add comentsfullpage

---
+++ src/comentsfullpage.jsx
@@ -0,0 +1,77 @@
+import "./css/comentsfullpage.css";
+import React, { useState } from 'react';
+
+const Formulario = () => {
+  const [thread, setThread] = useState({
+    thread_author: '',
+    thread_subject: '',
+    thread_title: '',
+    thread_msg: ''
+  });
+
+  const handleChange = e => {
+    setThread({ ...thread, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    console.log(thread);
+  };
+
+  return (
+    <>
+    <div class="caixaGrande d-flex justify-content-center align-item-center mb-5 p-3">
+    <div class="d-flex border border-dark p-3" id="container">
+
+    <form onSubmit={handleSubmit}>
+      <div class="d-flex align-items-start flex-column p-1">
+        <label htmlFor="thread_author"></label>
+        <input placeholder='Author'
+          type="text"
+          id="thread_author"
+          name="thread_author"
+          value={thread.thread_author}
+          onChange={handleChange}
+        />
+      </div>
+      <div class="d-flex align-items-start flex-column p-1">
+        <label htmlFor="thread_subject"></label>
+        <input placeholder='Subject'
+          type="text"
+          id="thread_subject"
+          name="thread_subject"
+          value={thread.thread_subject}
+          onChange={handleChange}
+          required
+        />
+      </div>
+      <div class="d-flex align-items-start flex-column p-1">
+        <label htmlFor="thread_title"></label>
+        <input placeholder='Title'
+          type="text"
+          id="thread_title"
+          name="thread_title"
+          value={thread.thread_title}
+          onChange={handleChange}
+          required
+        />
+      </div>
+      <div class="d-flex align-items-start flex-column pb-2">
+        <label htmlFor="thread_msg"></label>
+        <textarea placeholder="Digite sua mensagem"
+          id="thread_msg"
+          name="thread_msg"
+          value={thread.thread_msg}
+          onChange={handleChange}
+          required
+        />
+      </div>
+      <button type="button" class=" btn btn-outline-dark" id="botãoTeste">Submit</button>
+    </form>
+    </div>
+    </div>
+    </>
+  );
+};
+
+export default Formulario;
--- src/fullpage.jsx
+++ src/fullpage.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import "./css/fullpage.css";
+import Formulario from "./comentsfullpage";
 
 const FullPage = () => {
   const [isVisible, setIsVisible] = useState(false);
@@ -13,7 +14,7 @@ const FullPage = () => {
       {" "}
       <div>
         <div id="btnTesteId">
-        <button onClick={handleClick} id='btnTesteClass' >Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae, placeat eum id tempora, officia aut nulla labore dolorem sit cupiditate error? Qui perferendis blanditiis, nam totam maxime velit libero labore?</button>
+        <button onClick={handleClick} id='btnTesteClass' >Lorem ipsum dolor, sit amet consectetur adipisicing elit. Enim ea voluptatem dolore aut incidunt voluptatibus perspiciatis in nemo tenetur neque, qui perferendis temporibus explicabo possimus quasi veniam quibusdam. Quo, architecto.</button>
         </div>
         
         {isVisible && (
@@ -23,12 +24,12 @@ const FullPage = () => {
             </button>
             <div id="conteudo">
               <div className="conteudoPost"> 
-              <h6 className="nomeAuthor">nome</h6>
+              <h6 className="nomeAuthor">Nome</h6>
               <h1 className="Titulo">Titulo do assunto</h1>
               <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse dolor velit rem illum hic, illo voluptatem id ea doloribus exercitationem beatae ex omnis nihil dolores voluptates praesentium corporis eum perspiciatis! Lorem, ipsum dolor sit amet consectetur adipisicing elit. Optio sit modi perspiciatis enim eos molestiae ullam id, magnam nam, laudantium eveniet deserunt quas magni animi ea error vel, eaque laboriosam?Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis quibusdam rerum eum, illum hic nihil rem dolores, ex earum sequi vel consequuntur esse ipsa? Provident ad veniam doloremque voluptatum quaerat. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestias eos numquam sunt nisi eveniet provident amet ducimus, esse excepturi sit natus est. Officiis aspernatur, quas omnis similique odit tempora? Quae.</p>
               </div>
             </div>
-            <div className="coments"><h1>Comentarios</h1></div>
+            <Formulario/>
           </div>
         )}
       </div>
